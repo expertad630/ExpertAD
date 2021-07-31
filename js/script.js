072a3b644f68c76b693ebc60d333ba781a09b718
@expertad630 <ayandas630@gmail.com>
2021-08-01 02:09:51 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Script for NavBar
+function showmenu(){
+  var navlinks= document.getElementById("navlinks");
+    navlinks.style.right="0";
+  }
+  function hidemenu(){
+    var navlinks= document.getElementById("navlinks");
+    navlinks.style.right="-500px";
+  }
